f08343eb8a105d149df0683311ccab5f46bb83c4
Nader <njazari@gmail.com>
2016-10-06 00:25:25 +0000

    adding contoller and index route

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  get 'wall/index'
+
   mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root 'wall#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
