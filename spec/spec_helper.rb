19bfc13c9416edc8ee7bde0d8814c1740651cbd3
Nader <njazari@gmail.com>
2016-10-18 19:52:32 +0000

    remove features from coverage report

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,13 @@
 require 'simplecov'
-SimpleCov.start
+SimpleCov.start do 
+  add_filter '/spec/'
+  add_filter '/features/'
+  add_group 'Controllers', 'app/controllers'
+  add_group 'Models', 'app/models'
+  add_group 'Helpers', 'app/helpers'
+  add_group 'Mailers', 'app/mailers'
+  add_group 'Views', 'app/views'
+end
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
