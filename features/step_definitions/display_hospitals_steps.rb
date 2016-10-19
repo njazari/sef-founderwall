227f674e3b66c2f03a6b021b6e47af702801d3f3
Emmanuel Aguirre <eaaguirre@berkeley.edu>
2016-10-19 22:57:52 +0000

    delete ambiguous step definition for 'given the following hospital exists'

--- features/step_definitions/display_hospitals_steps.rb
+++ features/step_definitions/display_hospitals_steps.rb
@@ -1,9 +1,3 @@
-Given(/^the following hospitals exist\$$/) do |hospitals|
-  hospitals.hashes.each do |hospital|
-    Hospital.create!(hospital)
-  end
-end
-
 Then(/^I should see "([^"]*)" before "([^"]*)"$/) do |hospital1, hospital2|
   regexp = /#{hospital1}.*#{hospital2}/m
   page.body.should =~ regexp
