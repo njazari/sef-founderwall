8b43da0fa21cfa859b1187d689b11f0c22bff4cb
Ryan <ryankapur@berkeley.edu>
2016-10-20 06:47:09 +0000

    Maps cucumber prog

--- features/step_definitions/map_steps.rb
+++ features/step_definitions/map_steps.rb
@@ -1,18 +1,7 @@
-Given(/^the wall is set up$/) do
-end
-
-Given(/^the following hospitals exist\$$/) do |hospitals|
-  hospitals.hashes.each do |hospital|
-    Hospital.create!(hospital)
+Given(/^the following hospitals exist\$$/)  do |hospitals|
+  hospitals.hashes.each do |table|
+    Hospital.create!(table)
   end
 end
 
-Then(/^I should see the latitude$/) do
-end
 
-Then(/^I should see the longitude/) do
-end
-
-Then(/^I should see a pin on the map$/) do
-#   expect(page).to have_selector('#markers img', count: 1)
-end
