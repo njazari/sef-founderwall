edee1b25bd1592ca1f3405e1fefe351355ca3897
Nader <njazari@gmail.com>
2016-10-30 07:38:46 +0000

    admin redirect

--- app/models/donor.rb
+++ app/models/donor.rb
@@ -1,4 +1,4 @@
 class Donor < ActiveRecord::Base
-    has_many :dedications
+    has_many :dedications, :dependent => :destroy
     has_many :hospitals, :through => :dedications
 end
--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,5 +1,7 @@
 RailsAdmin.config do |config|
-
+  config.authorize_with do
+    redirect_to main_app.root_path unless current_user != nil and current_user.admin == true
+  end
   ### Popular gems integration
 
   ## == Devise ==
