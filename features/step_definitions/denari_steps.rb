391c1e5160d758d6740fae6db15c2a897047c2af
Nader <njazari@gmail.com>
2016-11-10 06:46:44 +0000

    denari cucumber scenarios

---
+++ features/step_definitions/denari_steps.rb
@@ -0,0 +1,27 @@
+Given(/^I send a GET request to denari for hospital with id "([^"]*)"$/) do |id|
+    pending
+end
+
+Then(/^the response status should be ([^"]*)"$/) do |status|
+    pending
+end
+
+Then(/^the JSON output should have hospital name ([^"]*)"$/) do |name|
+    pending
+end
+
+Given(/^I send a POST request to denari for hospital with new data "([^"]*)"$/) do |name|
+    pending
+end
+
+Given(/^I send a GET request to denari for donor with id "([^"]*)"$/) do |id|
+    pending
+end
+
+Then(/^the JSON output should have donor name ([^"]*)"$/) do |name|
+    pending
+end
+
+Given(/^I send a POST request to denari for donor with new data "([^"]*)"$/) do |name|
+    pending
+end
