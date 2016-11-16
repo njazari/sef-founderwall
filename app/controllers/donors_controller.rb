3ef4511778beb351ea912df3d39528c66d345954
Nahyun Kim <nahyunkim@outlook.com>
2016-11-16 21:39:48 +0000

    rebased, fixed donor_logged_in case where no user logged in

--- app/controllers/donors_controller.rb
+++ app/controllers/donors_controller.rb
@@ -4,7 +4,11 @@ class DonorsController < ApplicationController
         @tiers = Dedication.tiers
         @donor_nonempty_dedications_by_tier = @donor.nonempty_dedications_by_tier
         @donor_new_dedications = @donor.new_dedications
-        @donor_logged_in = current_user.donor == @donor
+        if current_user
+            @donor_logged_in = current_user.donor == @donor
+        else
+            @donor_logged_in = false
+        end
         if @donor.status == false
             flash[:error] = "donor"
             redirect_to errors_path
