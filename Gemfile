c4ecf911c15cddf8b5b5169b4bfd9fc19b852111
Nader <njazari@gmail.com>
2016-11-16 07:31:17 +0000

    sending emails works

--- Gemfile
+++ Gemfile
@@ -28,6 +28,7 @@ gem 'haml'
 gem 'devise'
 gem 'rails_admin'
 gem 'mail_form'
+gem 'simple_form'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161114060241) do
+ActiveRecord::Schema.define(version: 20161116070341) do
+
+  create_table "contact_forms", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "contactforms", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "dedications", force: :cascade do |t|
     t.string  "dedication"
