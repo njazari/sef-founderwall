4a7952d87db6242afd18c8e1ed6b8087cb669c19
Nader <njazari@gmail.com>
2016-11-17 07:21:10 +0000

    updated error controller

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,13 +1,7 @@
 class ErrorsController < ApplicationController
     def index
-        if flash[:error] == "hospital"
-            @error = "Hospital"
-            render 'status'
-        elsif flash[:error] == "donor"
-            @error = "Donor"
-            render 'status'
-        elsif flash[:error] == "dedication"
-            @error = "Dedication"
+        if flash[:error] == "hospital" or flash[:error] == "donor" or flash[:error] == "dedication"
+            @error = flash[:error]
             render 'status'
         elsif flash[:error] == "admin"
             render 'index'
