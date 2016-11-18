0c5b0e7694801d327117919708c965b0d8f3a6f5
Nader <njazari@gmail.com>
2016-11-18 21:13:51 +0000

    input validation works

--- app/controllers/contact_forms_controller.rb
+++ app/controllers/contact_forms_controller.rb
@@ -10,21 +10,21 @@ class ContactFormsController < ApplicationController
     def create
       begin
         @contact_form = ContactForm.new(params[:contact_form])
-        @contact_form.request = request
         @spam = false
         if not @contact_form.nickname.empty?
           @spam = true
           @message = "Sorry, this message appears to be spam and was not delivered."
         else
-          #if @contact_form.deliver # this line is here to trigger css styling/input validation
-            if @contact_form.send_message.code == 200
+          send_code = @contact_form.send_message
+          if send_code
+            if send_code.code == 200
               @message = 'Thank you for your message!'
             else
               @message = "Could not send message. Try again"
             end
-          # else
-          #   render :new
-          #end
+          else
+            render :new
+          end
       end
       rescue ScriptError
         flash[:error] = 'Sorry, this message appears to be spam and was not delivered.'
--- app/models/contact_form.rb
+++ app/models/contact_form.rb
@@ -9,14 +9,16 @@ class ContactForm < MailForm::Base
   attribute :nickname,  :captcha  => true
 
   def send_message
-    api_key = 'key-8ea371e6d27947f8585fabe38f3eaea3'
-    mailgun_domain = 'sandbox20eb7f8a89f94fd7a615b95e0ce9d583.mailgun.org'
-    RestClient.post "https://api:#{api_key}"\
-    "@api.mailgun.net/v3/#{mailgun_domain}/messages",
-    :from => "[#{name}] Wall of Founders Support <postmaster@#{mailgun_domain}>",
-    :to => "Sankara Eye Foundation <integrations@giftofvision.org>",
-    :subject => "WoF Support Email",
-    :text => "Message from #{name} (#{email}): \n\n #{message}"
+    if self.valid?
+      api_key = 'key-8ea371e6d27947f8585fabe38f3eaea3'
+      mailgun_domain = 'sandbox20eb7f8a89f94fd7a615b95e0ce9d583.mailgun.org'
+      RestClient.post "https://api:#{api_key}"\
+      "@api.mailgun.net/v3/#{mailgun_domain}/messages",
+      :from => "[#{name}] Wall of Founders Support <postmaster@#{mailgun_domain}>",
+      :to => "Sankara Eye Foundation <integrations@giftofvision.org>",
+      :subject => "WoF Support Email",
+      :text => "Message from #{name} (#{email}): \n\n #{message}"
+    end
   end
 
   # Declare the e-mail headers. It accepts anything the mail method
