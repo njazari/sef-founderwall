e619e41cbc019193f30f4472bbf9766f1d503b84
Emmanuel Aguirre <eaaguirre@berkeley.edu>
2016-11-25 23:28:18 +0000

    Add search_query scope for dedications; partially working

--- app/models/dedication.rb
+++ app/models/dedication.rb
@@ -29,6 +29,34 @@ class Dedication < ActiveRecord::Base
         end
     }
     
+    scope :search_query, lambda { |query|
+        # Searches the dedications table on the 'dedication' column.
+        # Matches using LIKE, automatically appends '%' to each term.
+        # LIKE is case INsensitive with MySQL, however it is case
+        # sensitive with PostGreSQL. To make it work in both worlds,
+        # we downcase everything.
+        return nil  if query.blank?
+
+        # condition query, parse into individual keywords
+        terms = query.downcase.split(/\s+/)
+
+        # replace "*" with "%" for wildcard searches,
+        # append '%', remove duplicate '%'s
+        terms = terms.map { |e|
+            (e.gsub('*', '%') + '%').gsub(/%+/, '%')
+        }
+        # configure number of OR conditions for provision
+        # of interpolation arguments. Adjust this if you
+        # change the number of OR conditions.
+        num_or_conds = 1
+        where(
+            terms.map { |term|
+            "(LOWER(dedications.dedication) LIKE ?)"
+            }.join(' AND '),
+            *terms.map { |e| [e] * num_or_conds }.flatten
+        )
+    }
+    
     def self.options_for_sorted_by 
         [
             ['Dedication', 'dedication_asc'],
