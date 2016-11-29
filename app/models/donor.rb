bce2f86196a68a79e896bf5f3314d2c615576f9e
Emmanuel Aguirre <eaaguirre@berkeley.edu>
2016-11-29 01:20:13 +0000

    add search functionality for dedications. works when sorted by donor

--- app/models/donor.rb
+++ app/models/donor.rb
@@ -1,7 +1,11 @@
+require 'will_paginate' 
 class Donor < ActiveRecord::Base
     has_many :dedications, :dependent => :destroy
     has_many :hospitals, :through => :dedications
     has_one :user
+    
+    filterrific(
+        available_filters: [:search_query])
     
     after_initialize do 
         if new_record? then if self.status.nil? then self.status = true end end end
@@ -11,7 +15,35 @@ class Donor < ActiveRecord::Base
                 dedication.update(status: self.status)
             end 
         end
-    end
+    end 
+    
+    scope :search_query, lambda { |query|
+        # Searches the dedications table on the 'dedication' column.
+        # Matches using LIKE, automatically appends '%' to each term.
+        # LIKE is case INsensitive with MySQL, however it is case
+        # sensitive with PostGreSQL. To make it work in both worlds,
+        # we downcase everything.
+        return nil  if query.blank?
+
+        # condition query, parse into individual keywords
+        terms = [query.downcase]
+
+        # replace "*" with "%" for wildcard searches,
+        # append '%', remove duplicate '%'s
+        terms = terms.map { |e|
+            ('%' + e + '%')
+        }
+        # configure number of OR conditions for provision
+        # of interpolation arguments. Adjust this if you
+        # change the number of OR conditions.
+        num_or_conds = 2
+        where(
+            terms.map { |term|
+            "(LOWER(donors.first_name) LIKE ? OR LOWER(donors.last_name) LIKE ?)"
+            }.join(' AND '),
+            *terms.map { |e| [e] * num_or_conds }.flatten
+        )
+    }
     
     def dedications_by_tier(published)
         dbt = Hash.new
